855d2b61abb91c43ab84b7b216f62037af0b408d
krolhm <raphaelb1@icloud.com>
2023-10-03 15:28:03 +0200

    adding emulator packages requirements

--- init_aosp.sh
+++ init_aosp.sh
@@ -11,6 +11,7 @@ mkdir aosp13
 cd aosp13
 git config --global user.name RandomSith
 git config --global user.email randomsith@gmail.com
-sudo apt install repo libncurses5 -y
+sudo apt update
+sudo apt install repo libncurses5 zip libpulse0 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libasound2 -y
 y | repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r1
 repo sync -c -j"${j}"
